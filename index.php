4ff4de79714c5225685e260157cb2570cace6eb2
Delly431 <89039935+Delly431@users.noreply.github.com>
2021-08-16 14:40:13 -0400

    A lot of updates.

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
-<?php 
+<?php
   // Let's include any helper classes for us to use in our markup.
-  include 'application/includes/helper_includes.php'; 
+  include 'application/includes/helper_includes.php';
   session_start();
- 
+
   // If we don't pass in an id argument then let's assume
   // were one the home page.
   if (empty($_GET['id'])) {
@@ -14,33 +14,37 @@
   // Start of Page Markup Generation
   include("application/views/header.php");
     // Pull the id we pass to know which page to load.
-    switch($_GET['id']) { 
-      default: 
+    switch($_GET['id']) {
+      default:
       include ('application/views/home.php');
       break;
 
-      case "roster": 
+      case "roster":
       include ('application/views/roster.php');
       break;
 
-      case "rank": 
-      include ('application/views/rank.php'); 
+      case "rank":
+      include ('application/views/rank.php');
       break;
 
-      case "servers": 
-      include ('application/views/servers.php'); 
+      case "servers":
+      include ('application/views/servers.php');
       break;
 
-      case "laws": 
-      include ('application/views/laws.php'); 
-      break;
-	  
-	  case "wow": 
-      include ('application/views/wow.php'); 
+      case "laws":
+      include ('application/views/laws.php');
       break;
 
-      case "about": 
-      include ('application/views/about.php'); 
+      case "awards": 
+      include ('application/views/awards.php');
+      break;
+
+	  case "wow":
+      include ('application/views/wow.php');
+      break;
+
+      case "about":
+      include ('application/views/about.php');
       break;
     }
   include("application/views/footer.php");
